0dc78b05daad7c36fe4fd4296d06e25b4421fa0c
Li Zhe Yun <93893933+WindowsCommandPrompt@users.noreply.github.com>
2022-05-07 21:46:54 +0800

    Create f2.java

---
+++ app/src/main/java/sg/np/edu/mad/messagegroup/f2.java
@@ -0,0 +1,17 @@
+package sg.np.edu.mad.messagegroup;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class f2 extends Fragment {
+    //pump controls Fragment 2
+    @Override
+    public View onCreateView(LayoutInflater pump, ViewGroup cluster, Bundle savedInstanceState){
+        View v = pump.inflate(R.layout.f2, cluster, false);
+        return v;
+    }
+}
